47aaca7545151b4dc9c7f6ceaa193da2b55daec4
sonali1823 <vivsona1823@gmail.com>
2024-08-20 02:42:08 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,23 +4,23 @@ import './globals.css'; // Import your global styles
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+     <html lang="en">
       <body className="font-sans bg-gray-100">
-        <nav className="bg-blue-600 text-white p-4">
+        <nav className="bg-pink text-white p-4">
           <ul className="flex space-x-4">
             <li>
               <Link href="/menu"
-              className="hover:bg-blue-700 p-2 rounded">Menu
+              className="hover:bg-black p-2 rounded">Menu
               </Link>
             </li>
             <li>
               <Link href="/aboutus"
-               className="hover:bg-blue-700 p-2 rounded">About Us
+               className="hover:bg-black p-2 rounded">About Us
               </Link>
             </li>
             <li>
               <Link href="/contactus"
-                className="hover:bg-blue-700 p-2 rounded">Contact Us
+                className="hover:bg-black p-2 rounded">Contact Us
               </Link>
             </li>
           </ul>
